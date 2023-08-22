1969ff873f7692ce2434d397b0f84bbe0b286291
hizkiastesfaye <tesfayehizkias@gmail.com>
2023-08-22 13:11:31 +0300

    rectangle 0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// createing new empty class
+class Rectangle {
+  // empty class
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,4 @@
+0x13-javascript_objects_scopes_closures
+
+# JAVASCRIPT OBJECTS COPES CLOSERUES
+. 0-rectangle.js
